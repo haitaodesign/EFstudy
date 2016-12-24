0b80ede82b61cfccec4a447462c2ac3fc765ecc9
haitaodesign <15393243613@163.com>
2016-12-24 21:32:15 +0800

    实现简单的CRUD接口

---
+++ EFDbApp/DataAccessLayer/ISimplestOrderClientRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DataAccessLayer
+{
+    /// <summary>
+    /// 实现最简单CRUD接口，具体的操作类实现此接口
+    /// </summary>
+    public interface ISimplestOrderClientRepository
+    {
+        List<OrderClient> GetAllClients();
+
+        List<OrderClient> FindClientsByName(String FindWhat);
+
+        int AddClient(OrderClient client);
+
+        int DeleteClient(OrderClient ClientID);
+
+        int ModifyClient(OrderClient client);
+    }
+}

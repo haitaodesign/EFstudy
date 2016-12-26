c181f9fd7c1a05aba7b2cd733a49240bd40547e0
haitaodesign <15393243613@163.com>
2016-12-26 16:30:44 +0800

    编写获得所有客户信息的测试方法

---
+++ EFDbApp/DALTestProject/SimplestOrderClientRepositoryTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using Model;
+
+namespace DALTestProject
+{
+    [TestClass]
+    public class SimplestOrderClientRepositoryTest
+    {
+        ISimplestOrderClientRepository repo = null;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            repo = new SimplestOrderClientRepository();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            //单元测试运行完之后不做任何操作
+        }
+
+
+        [TestMethod]
+        public void TestGetAllClients()
+        {
+            List<OrderClient> Clients = repo.GetAllClients();
+            Assert.IsTrue(Clients.Count() > 0);
+        }
+
+
+
+    }
+}
---
+++ EFDbApp/DataAccessLayer/SimplestOrderClientRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DataAccessLayer
+{
+    public class SimplestOrderClientRepository : ISimplestOrderClientRepository
+    {
+        public int AddClient(OrderClient client)
+        {
+            if(client == null)
+            {
+                return 0;
+            }
+            using (var context = new MyDBEntities())
+            {
+                context.OrderClients.Add(client);
+                return context.SaveChanges();
+            }
+        }
+
+        public int DeleteClient(OrderClient ClientID)
+        {
+            using (var context = new MyDBEntities())
+            {
+                var client = context.OrderClients.Find(ClientID);
+                if (client != null)
+                {
+                    context.OrderClients.Remove(client);
+                    return context.SaveChanges();
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public List<OrderClient> FindClientsByName(string FindWhat)
+        {
+            using (var context=new MyDBEntities())
+            {
+                return context.OrderClients.Where(c => c.ClientName.StartsWith(FindWhat)).ToList();
+            }
+        }
+
+        public List<OrderClient> GetAllClients()
+        {
+            using (var context = new MyDBEntities())
+            {
+                return context.OrderClients.ToList();
+            }
+        }
+
+        public int ModifyClient(OrderClient client)
+        {
+            if (client == null)
+            {
+                return 0;
+            }
+            using (var context = new MyDBEntities())
+            {
+                var tempClient = context.OrderClients.FirstOrDefault(c => c.ClientID == client.ClientID);
+                if (tempClient != null)
+                {
+                    tempClient.Address = client.Address;
+                    tempClient.ClientName = client.ClientName;
+                    tempClient.Email = client.Email;
+                    tempClient.PostCode = client.PostCode;
+                    tempClient.Telephone = client.Telephone;
+                    return context.SaveChanges();
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}

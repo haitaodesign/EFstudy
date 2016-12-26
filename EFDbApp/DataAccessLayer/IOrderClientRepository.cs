23c3e28e03d0b313a37a4551db694242bb822ff2
haitaodesign <15393243613@163.com>
2016-12-26 18:06:32 +0800

    实现一个需要手工Commit,支持同步/异步操作和Dispose的Respository

---
+++ EFDbApp/DataAccessLayer/IOrderClientRepository.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALTestProject
+{
+    /// <summary>
+    /// 一个需要手工Commit，支持同步/异步操作和Dispose的Repository，
+    /// </summary>
+    interface IOrderClientRepository
+    {
+        Task<List<OrderClient>> GetAllClientsAsync();
+
+        List<OrderClient> GetAllClients();
+
+        void AddClient(OrderClient client);
+
+        void DeleteClient(int ClientID);
+
+        void ModifyClient(OrderClient client);
+    }
+}
